8f9c0574a5647b3eb6ee70d4c538612b2dfb05ab
juvadev <juvaindiemaker@gmail.com>
2024-11-09 13:20:41 -0500

    Add dashboard cards for total revenue, gross margin, sales, and low stock products

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { Boxes, DollarSign, Percent, ShoppingBasket } from "lucide-react";
 
 export function Dashboard() {
   return (
@@ -18,14 +20,69 @@ export function Dashboard() {
             Tablero
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+        <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+            <Card x-chunk="Displays total revenue in PEN and the percentage change compared to the previous month.">
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Ingresos Totales
+                </CardTitle>
+                <DollarSign className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">S/ 2,231.89</div>
+                <p className="text-xs text-muted-foreground">
+                  +20.1% respecto al mes pasado
+                </p>
+              </CardContent>
+            </Card>
+
+            <Card x-chunk="Shows the gross profit margin and the percentage increase compared to last month.">
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Margen Bruto de Beneficio
+                </CardTitle>
+                <Percent className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">+30%</div>
+                <p className="text-xs text-muted-foreground">
+                  +80.1% respecto al mes pasado
+                </p>
+              </CardContent>
+            </Card>
+
+            <Card x-chunk="Displays total sales and the percentage increase compared to the previous month.">
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">Ventas</CardTitle>
+                <ShoppingBasket className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">+12,234</div>
+                <p className="text-xs text-muted-foreground">
+                  +19% respecto al mes pasado
+                </p>
+              </CardContent>
+            </Card>
+
+            <Card x-chunk="Displays low stock products and the increase in the number of items since the last hour.">
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  Productos con Bajo Stock
+                </CardTitle>
+                <Boxes className="h-4 w-4 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">+23</div>
+                <p className="text-xs text-muted-foreground">
+                  <p className="text-xs text-muted-foreground">
+                    Riesgo de quedarse sin stock
+                  </p>
+                </p>
+              </CardContent>
+            </Card>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   );
